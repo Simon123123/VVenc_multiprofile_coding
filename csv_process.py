473493d6475e91c74541234a143f186450200f90
yliu <y.liu@ateme.com>
2023-05-19 12:32:29 +0200

    Code modif for fast and faster config

--- csv_process.py
+++ csv_process.py
@@ -16,10 +16,11 @@ def main(argv):
     height = 1080
     num_f = 1
     path = '.'
+    ctu_size = 128
     try:
-        opts, args = getopt.getopt(argv,"w:h:f:p:")
+        opts, args = getopt.getopt(argv,"w:h:f:p:", ["ctu_size="])
     except getopt.GetoptError:
-        print ('csv_process.py -w <width_frame> -h <height_frame> -f <number_frame> -p <path_csv_files>')
+        print ('csv_process.py -w <width_frame> -h <height_frame> -f <number_frame> -p <path_csv_files> --ctu_size <ctu size>')
         sys.exit(2)
     for opt, arg in opts:      
         if opt == "-w":
@@ -30,31 +31,31 @@ def main(argv):
             num_f = int(arg)
         elif opt == '-p':
             path = arg
+        elif opt == '--ctu_size':
+            ctu_size = int(arg)
+            
 
 
 
-    bord_w = int(width/128)*128
-    bord_h = int(height/128)*128
+    bord_w = int(width/ctu_size)*ctu_size
+    bord_h = int(height/ctu_size)*ctu_size
 
-    size_npy = int(width/128) * int(height/128) * num_f
+    dim_w = int(width/ctu_size)
+    
+    dim_h = int(height/ctu_size)
+    
 
-    # list_files_ctu = []
     list_files_trace = []
-
-    # for dirpath, dirnames, filenames in sorted(os.walk(path)):
-    #     for filename in [f for f in filenames if f.startswith("CTU_")]:
-    #         list_files_ctu.append(os.path.join(dirpath, filename))
 
     for dirpath, dirnames, filenames in sorted(os.walk(path)):
         for filename in [f for f in filenames if f.startswith("trace_")]:
             list_files_trace.append(os.path.join(dirpath, filename))
 
-
+    print (list_files_trace)
 
     for f in list_files_trace:
         
-        
-        ind_global_trace = 0
+    
         
         # ind_global_ctu = 0
     
@@ -66,30 +67,26 @@ def main(argv):
         
         # ctu.fill(-1)
     
-        qt_map = np.empty((size_npy, 8, 8, 1), dtype=np.int8)
+        size_qt = int(ctu_size / 16)
+        size_mt = int(ctu_size / 4)
+        
+        
+        qt_map = np.empty((num_f, dim_h, dim_w, 1, size_qt, size_qt), dtype=np.int8)
     
         qt_map.fill(-1)
     
-        mtdepth_map = np.empty((size_npy, 32, 32, 1), dtype=np.int8)
+        mtdepth_map = np.empty((num_f, dim_h, dim_w, 1, size_mt, size_mt), dtype=np.int8)
     
         mtdepth_map.fill(-1)
     
-        btdepth_map = np.empty((size_npy, 32, 32, 1), dtype=np.int8)
+        btdepth_map = np.empty((num_f, dim_h, dim_w, 1, size_mt, size_mt), dtype=np.int8)
     
         btdepth_map.fill(-1)
     
-        cushape_map = np.empty((size_npy, 32, 32, 2), dtype=np.int8)
+        cushape_map = np.empty((num_f, dim_h, dim_w, 2, size_mt, size_mt), dtype=np.int16)
     
         cushape_map.fill(-1)
     
-    
-        # mt1_map = np.empty((size_npy, 32, 32, 1), dtype=np.int8)
-    
-        # mt1_map.fill(-1)
-    
-        # mt2_map = np.empty((size_npy, 32, 32, 1), dtype=np.int8)
-    
-        # mt2_map.fill(-1)        
         
         print(os.path.join(os.path.dirname(filename), 'qt_map_{}.npy'.format(filename)))        
         
@@ -101,95 +98,55 @@ def main(argv):
             if r[1] >= bord_w or r[2] >= bord_h:
                 continue
             
+            ind_h = int(r[2] / ctu_size)
+            ind_w = int(r[1] / ctu_size)
             
-            # mt_dep = int(r[5]) >> int(5*r[6])
-                
-            # mt1_split = mt_dep & 31
-            # mt2_split = (mt_dep >> 5) & 31
+            ind_poc = int(r[0])
+            
+            ref_x_qt = int((r[1] % ctu_size) / 16)
+            ref_y_qt = int((r[2] % ctu_size) / 16)
 
-
-            ref_x_qt = int((r[1] % 128) / 16)
-            ref_y_qt = int((r[2] % 128) / 16)
-
-            ref_x_mt = int((r[1] % 128) / 4)
-            ref_y_mt = int((r[2] % 128) / 4)
+            ref_x_mt = int((r[1] % ctu_size) / 4)
+            ref_y_mt = int((r[2] % ctu_size) / 4)
             
 
-            if (r[1] + r[3]) % 128 == 0:
-                dx_qt = 8 - ref_x_qt
-                dx_mt = 32 - ref_x_mt
+            if (r[1] + r[3]) % ctu_size == 0:
+                dx_qt = size_qt - ref_x_qt
+                dx_mt = size_mt - ref_x_mt
             else:    
-                dx_mt = int(((r[1] + r[3]) % 128) / 4) - ref_x_mt
-                dx_qt = int(((r[1] + r[3] + 12) % 128) / 16) - ref_x_qt
+                dx_mt = int(((r[1] + r[3]) % ctu_size) / 4) - ref_x_mt
+                dx_qt = int(((r[1] + r[3] + 12) % ctu_size) / 16) - ref_x_qt
                 
-            if (r[2] + r[4]) % 128 == 0:   
-                dy_qt = 8 - ref_y_qt
-                dy_mt = 32 - ref_y_mt
+            if (r[2] + r[4]) % ctu_size == 0:   
+                dy_qt = size_qt - ref_y_qt
+                dy_mt = size_mt - ref_y_mt
 
             else:
-                dy_qt = int(((r[2] + r[4] + 12) % 128) / 16) - ref_y_qt
-                dy_mt = int(((r[2] + r[4]) % 128) / 4) - ref_y_mt
+                dy_qt = int(((r[2] + r[4] + 12) % ctu_size) / 16) - ref_y_qt
+                dy_mt = int(((r[2] + r[4]) % ctu_size) / 4) - ref_y_mt
 
 
             for i in range(dx_qt):
                 for j in range(dy_qt):
-                    assert (qt_map[ind_global_trace, ref_y_qt + j, ref_x_qt + i, :] == -1 or qt_map[ind_global_trace, ref_y_qt + j, ref_x_qt + i, :] == r[7]), "The qt size is not coherent"
-                    qt_map[ind_global_trace, ref_y_qt + j, ref_x_qt + i, :] = r[7] 
+                    assert (qt_map[ind_poc, ind_h, ind_w, :, ref_y_qt + j, ref_x_qt + i] == -1 or qt_map[ind_poc, ind_h, ind_w, :, ref_y_qt + j, ref_x_qt + i] == r[7]), "The qt size is not coherent"
+                    qt_map[ind_poc, ind_h, ind_w, :, ref_y_qt + j, ref_x_qt + i] = r[7] 
             
             
             for i in range(dx_mt):
                 for j in range(dy_mt):
 
 
-                    assert (mtdepth_map[ind_global_trace, ref_y_mt + j, ref_x_mt + i, :] == -1), "The mtdepth is not coherent"
-                    assert (btdepth_map[ind_global_trace, ref_y_mt + j, ref_x_mt + i, :] == -1), "The btdepth is not coherent"
+                    assert (mtdepth_map[ind_poc, ind_h, ind_w, :, ref_y_mt + j, ref_x_mt + i] == -1), "The mtdepth is not coherent"
+                    assert (btdepth_map[ind_poc, ind_h, ind_w, :, ref_y_mt + j, ref_x_mt + i] == -1), "The btdepth is not coherent"
                     
-                    assert (cushape_map[ind_global_trace, ref_y_mt + j, ref_x_mt + i, 0] == -1), "The cushape is not coherent"
+                    assert (cushape_map[ind_poc, ind_h, ind_w, 0, ref_y_mt + j, ref_x_mt + i] == -1), "The cushape is not coherent"
 
-                    mtdepth_map[ind_global_trace, ref_y_mt + j, ref_x_mt + i, :] = r[8] 
-                    btdepth_map[ind_global_trace, ref_y_mt + j, ref_x_mt + i, :] = r[9]
-                    cushape_map[ind_global_trace, ref_y_mt + j, ref_x_mt + i, 0] = r[3] 
-                    cushape_map[ind_global_trace, ref_y_mt + j, ref_x_mt + i, 1] = r[4]
+                    mtdepth_map[ind_poc, ind_h, ind_w, :, ref_y_mt + j, ref_x_mt + i] = r[8] 
+                    btdepth_map[ind_poc, ind_h, ind_w, :, ref_y_mt + j, ref_x_mt + i] = r[9]
+                    cushape_map[ind_poc, ind_h, ind_w, 0, ref_y_mt + j, ref_x_mt + i] = r[3] 
+                    cushape_map[ind_poc, ind_h, ind_w, 1, ref_y_mt + j, ref_x_mt + i] = r[4]
                     
 
-
-                    # assert (mt1_map[ind_global_trace, ref_y_mt + j, ref_x_mt + i, :] == -1), "The mt1 decision is not coherent"
-                    # assert (mt2_map[ind_global_trace, ref_y_mt + j, ref_x_mt + i, :] == -1), "The mt2 decision is not coherent"
-
-                    # mt1_map[ind_global_trace, ref_y_mt + j, ref_x_mt + i, :] = mt1_split
-                    # mt2_map[ind_global_trace, ref_y_mt + j, ref_x_mt + i, :] = mt2_split
-
-
-            if (r[1] + r[3]) % 128 == 0 and (r[2] + r[4]) % 128 == 0:
-
-                ind_global_trace += 1                
-                # if (ind_global_trace % int(1e5) == 0):
-                #     print("Treating partition maps on line :", ind_global_trace)                
-                
-
-
-
-    # for f in list_files_ctu:
-    
-    #     ctu_file = pd.read_csv(f, delimiter=';', header = None, keep_default_na=False).to_numpy()
-
-    #     for r in ctu_file: 
-            
-    #         assert (ctu[ind_global_ctu, 0, 0, 0] == -1), "The ctu value is not coherent"
-    #         ctu[ind_global_ctu, :, :, :] = np.array(r[3:-1].reshape(128, 128, 1)/1024, dtype = np.float16)
-            
-    #         ind_global_ctu += 1
-
-
-    # np.save( os.path.join(path, 'ctu.npy'), ctu)
-
-    # np.save( os.path.join(path, 'qt_map.npy'), qt_map)
-
-    # np.save( os.path.join(path, 'mt1_map.npy'), mt1_map)
-
-    # np.save( os.path.join(path, 'mt2_map.npy'), mt2_map)
-    
-    
 
         np.save( os.path.join(os.path.dirname(f), 'qt_map_{}.npy'.format(filename)), qt_map)
     
@@ -199,27 +156,7 @@ def main(argv):
         
         np.save( os.path.join(os.path.dirname(f), 'cushape_map_{}.npy'.format(filename)), cushape_map)        
         
-    
-    
-        print("Number of extracted CTUs is : ", ind_global_trace)
-    
-        assert (ind_global_trace == size_npy), "The number of extracted partition samples is not coherent with the number of input frames"
-    
-        # assert (ind_global_ctu == size_npy), "The number of extracted CTU samples is not coherent with the number of input frames"
-    
-    
-    
-        # if -1 in ctu:
-        #     raise Exception('-1 value is in CTU pixels')
-    
-        # if -1 in qt_map:
-        #     raise Exception('-1 value is in QTdepth map')
-    
-        # if -1 in mt1_map:
-        #     raise Exception('-1 value is in mt1 map')
-    
-        # if -1 in mt2_map:
-        #     raise Exception('-1 value is in mt2 map')
+
     
     
         if -1 in qt_map:
--- scripts/get_metric_map_from_npy.py
+++ scripts/get_metric_map_from_npy.py
@@ -2,12 +2,12 @@ import math
 import os
 import argparse
 import numpy as np
-from common import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--npy', help='numpy array file to parse', type=str, default='cushape_map_trace_bqsquare_qp22.npy')
 parser.add_argument('--path', help='Directory to generated maps', type=str, default='data')
 parser.add_argument('--cell_size', help='Cell size of the CTU map', type=int, default=8)
+parser.add_argument('--ctu_size', help='CTU size', type=int, default=128)
 parser.add_argument('--metric', help='Metric to record from log (max_size_map_1d, max_size_map_2d, min_size_map_1d, min_size_map_2d, what else?)', type=str, default='max_size_1d')
 args = parser.parse_args()
 
@@ -28,22 +28,22 @@ def main():
         
         filename = f.split(os.sep)[-1].split('.')[0]
         
-        data = np.load(f).astype(np.int8)
+        data = np.load(f).astype(np.int16)
         
-        shape_w = data[..., 0]
+        shape_w = data[:, :, :, 0, :, :]
         
-        shape_h = data[..., 1]
+        shape_h = data[:, :, :, 1, :, :]
         
-        size_map = 32 * 4  // args.cell_size 
+        size_map = args.ctu_size // args.cell_size 
         
         ops_scale = args.cell_size // 4
         
         if args.metric in ['max_size_map_1d', 'max_size_map_2d']:
-            shape_w = shape_w.reshape((-1, size_map, ops_scale, size_map, ops_scale)).max(axis=(2, 4))
-            shape_h = shape_h.reshape((-1, size_map, ops_scale, size_map, ops_scale)).max(axis=(2, 4))
+            shape_w = shape_w.reshape((shape_w.shape[0], shape_w.shape[1], shape_w.shape[2], size_map, ops_scale, size_map, ops_scale)).max(axis=(4, 6))
+            shape_h = shape_h.reshape((shape_w.shape[0], shape_w.shape[1], shape_w.shape[2], size_map, ops_scale, size_map, ops_scale)).max(axis=(4, 6))
         else:
-            shape_w = shape_w.reshape((-1, size_map, ops_scale, size_map, ops_scale)).min(axis=(2, 4))
-            shape_h = shape_h.reshape((-1, size_map, ops_scale, size_map, ops_scale)).min(axis=(2, 4))        
+            shape_w = shape_w.reshape((shape_w.shape[0], shape_w.shape[1], shape_w.shape[2], size_map, ops_scale, size_map, ops_scale)).min(axis=(4, 6))
+            shape_h = shape_h.reshape((shape_w.shape[0], shape_w.shape[1], shape_w.shape[2], size_map, ops_scale, size_map, ops_scale)).min(axis=(4, 6))        
         
         
         shape = 0
@@ -53,7 +53,7 @@ def main():
             shape = np.minimum(shape_w, shape_h)
         else:
             # shape = np.concatenate((shape_w, shape_h), axis = -1)
-            shape = np.stack((shape_w, shape_h), axis = 1)
+            shape = np.stack((shape_w, shape_h), axis = 3)
         
         shape = shape.reshape(-1, size_map*size_map)
         np.savetxt(os.path.join(args.path, args.metric + '_scale_' + str(args.cell_size) + '_' + filename + '.csv'), shape, fmt='%d', delimiter=';')
